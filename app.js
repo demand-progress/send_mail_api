f97bcfb7999d84d4013a31468c4f8160a10725ba
Mateo Balcorta <misterrattail@gmail.com>
2018-08-01 12:18:16 -0700

    Enable all cors request.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors(corsOptions));
 // app.options('*', cors());
-app.use(cors());
+// app.use(cors());
 app.options('*', cors());
 
 routes(app);
