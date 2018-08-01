ac70041af04673517d63fbb50856e8293a887cea
Mateo Balcorta <misterrattail@gmail.com>
2018-08-01 11:39:19 -0700

    Updated options function.

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors(corsOptions));
 // app.options('*', cors());
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
-    next();
-});
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
 
 routes(app);
 
