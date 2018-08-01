7929ea864db65a7768335382c619a360c926c865
Mateo Balcorta <misterrattail@gmail.com>
2018-08-01 10:54:09 -0700

    Added headers.

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors(corsOptions));
 // app.options('*', cors());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET');
+    next();
+});
+
 routes(app);
 
 const port = process.env.PORT || 3000;
