3bc28e46490202e658ef5e7581899e1d0ba64dc3
Mateo Balcorta <misterrattail@gmail.com>
2018-08-01 11:59:29 -0700

    Add cors.

--- app.js
+++ app.js
@@ -14,13 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors(corsOptions));
 // app.options('*', cors());
-
-app.options("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.send(200);
-  });
+app.use(cors());
+app.options('*', cors());
 
 routes(app);
 
