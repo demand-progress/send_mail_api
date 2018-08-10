6fb3a530fc2bbdf696e35ee8fd6ccf93d2e6c3fc
Mateo Balcorta <misterrattail@gmail.com>
2018-08-10 14:57:43 -0700

    Added console log for logging in heroku.

--- commentFunc.js
+++ commentFunc.js
@@ -1,21 +1,21 @@
 const puppeteer = require('puppeteer');
-
 const postComment = (reqBody) => {
     return new Promise(async (resolve, reject) => {
         const { first_name, last_name, email, zip } = reqBody
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         })
+        console.log(reqBody)
         const page = await browser.newPage()
         try {
             await page.goto('https://www.regulations.gov/comment?D=USBC-2018-0005-0001', { waitUntil: 'networkidle0' })
             await page.type('#x-auto-0-input', first_name)
             await page.type('#x-auto-1-input', last_name)
             await page.type('#x-auto-2-input', email)
-            await page.type('#x-auto-5-input', 'Oakland')
-            await page.type('#x-auto-6-input', 'California')
+            await page.type('#x-auto-5-input', city)
+            await page.type('#x-auto-6-input', state)
             await page.type('#x-auto-8-input', zip)
-            await page.type('#x-auto-10-input', 'mateo@demandprogress.org')
+            await page.type('#x-auto-10-input', email)
             await page.evaluate(form => {
                 console.log('form submitted')
                 form.click()
