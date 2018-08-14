f8706d70bf91accc6ea98760daf2af8b1005ad96
Mateo Balcorta <misterrattail@gmail.com>
2018-08-14 15:44:23 -0700

    Added state.

--- commentFunc.js
+++ commentFunc.js
@@ -3,11 +3,6 @@ const axios = require('axios');
 var parseString = require('xml2js').parseString;
 var stateAbbreviations = require('states-abbreviations');
 
-
-// var xml = '<CityStateLookupRequest USERID="661DEMAN2298"><ZipCode ID="0"><Zip5>94601</Zip5></ZipCode></CityStateLookupRequest>'
-// var serialized = new XMLSerializer().serializeToString(xml);
-// console.log(serialized)
-
 const getState = (zip) => {
     return new Promise(async (resolve, reject) => {
         axios.get('https://secure.shippingapis.com/ShippingAPI.dll?', {
@@ -36,43 +31,39 @@ const postComment = (reqBody) => {
     return new Promise(async (resolve, reject) => {
         const { first_name, last_name, email, zip, fcc_comment } = reqBody
         const cityStateObj = await getState(zip)
-        const { stateFullName} = cityStateObj
-        await console.log(stateFullName, reqBody)
-        // const browser = await puppeteer.launch({
-        //     args: ['--no-sandbox', '--disable-setuid-sandbox']
-        // })
-        // console.log(reqBody)
+        const {stateFullName} = cityStateObj
 
-       
-        // const page = await browser.newPage()
-        // try {
-        //     await page.goto('https://www.regulations.gov/comment?D=FTC-2018-0049-0001', { waitUntil: 'networkidle0' })
-        //     await page.type('#x-auto-0-input', fcc_comment )
-        //     await page.type('#x-auto-1-input', first_name)
-        //     await page.type('#x-auto-2-input', last_name)
-        //     await page.type('#x-auto-6-input', stateFullName)
-        //     await page.type('#x-auto-8-input', zip)
-        //     await page.type('#x-auto-10-input', email)
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        })
 
-        //     await page.evaluate(form => {
-        //         form.click()
-        //     },  (await page.$x('/html/body/div[3]/div[2]/div[2]/div[3]/div/div[3]/div[1]/form/div[5]/span/button'))[0])
-        //     await page.click('#gwt-uid-250')
-        //     await page.click('#gwt-uid-382')
-        //     await page.waitFor(3000)
-        //     await page.type('#x-auto-21-input', email)
-        //     await page.evaluate(form => {
-        //         form.click()
-        //     },  (await page.$x('/html/body/div[3]/div[2]/div[2]/div[3]/div/div[3]/div[3]/div[1]/div/div[1]/button'))[0])
-        //     await browser.close();
-        //     await resolve('completed')
-    //         }  catch (error) {
-    //             reject(error);
-    //             browser.close();
-    //         }
+        const page = await browser.newPage()
+        try {
+            await page.goto('https://www.regulations.gov/comment?D=FTC-2018-0049-0001', { waitUntil: 'networkidle0' })
+            await page.type('#x-auto-0-input', fcc_comment )
+            await page.type('#x-auto-1-input', first_name)
+            await page.type('#x-auto-2-input', last_name)
+            await page.type('#x-auto-6-input', stateFullName.trim())
+            await page.type('#x-auto-8-input', zip)
+            await page.type('#x-auto-10-input', email)
+
+            await page.evaluate(form => {
+                form.click()
+            },  (await page.$x('/html/body/div[3]/div[2]/div[2]/div[3]/div/div[3]/div[1]/form/div[5]/span/button'))[0])
+            await page.click('#gwt-uid-250')
+            await page.click('#gwt-uid-382')
+            await page.waitFor(3000)
+            await page.type('#x-auto-21-input', email)
+            await page.evaluate(form => {
+                form.click()
+            },  (await page.$x('/html/body/div[3]/div[2]/div[2]/div[3]/div/div[3]/div[3]/div[1]/div/div[1]/button'))[0])
+            await browser.close();
+            await resolve('completed')
+            }  catch (error) {
+                reject(error);
+                browser.close();
+            }
       });
 }
-
-// postComment({ first_name: 'me', last_name: 'you', email: 'mateo@demandprogress.org', zip: '94601', input_text: 'hello world' } )
 
 module.exports = postComment
