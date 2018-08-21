3b661af3bd309678df1dc0dc1cf6c0c9b1c90f1e
Mateo Balcorta <misterrattail@gmail.com>
2018-08-21 10:47:26 -0700

    Clean up.

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,9 @@
-const postComment = require('../commentFunc.js')
+const postComment = require('../commentFunc.js');
 
-var appRouter = function (app) {
-    app.get("/", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
-    });
-    app.post("/comment", function(req, res) {
-        return postComment(req.body).then((answer) => res.send(answer))  
-      });
-  }
-  
-  module.exports = appRouter;
+const appRouter = (app) => {
+  app.get('/', (req, res) => {
+    res.status(200).send('Welcome to our restful API');
+  });
+  app.post('/comment', (req, res) => postComment(req.body).then(answer => res.send(answer)));
+};
+module.exports = appRouter;
