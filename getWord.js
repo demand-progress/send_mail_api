95e9f74b98099bd06bff55c2048dccc9a7f1d71d
Mateo Balcorta <misterrattail@gmail.com>
2018-08-26 18:57:06 -0700

    Update params.

--- getWord.js
+++ getWord.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const getWord = (difficulty, start, count) => new Promise((resolve, reject) => {
   axios.get('http://app.linkedin-reach.io/words', {
     params: {
-      difficulty: difficulty,
-      start: start,
-      count: count
+      difficulty: 1,
+      start: 1,
+      count: 1
     }
   })
     .then((response) => {
