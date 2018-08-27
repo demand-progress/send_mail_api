16b04db9ab19eb261cad6ff1bab45b45897348df
Mateo Balcorta <misterrattail@gmail.com>
2018-08-26 19:10:49 -0700

    Update request query variables as constants.

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const getWord = require('../getWord.js');
 
 const appRouter = (app) => {
   app.get('/', (req, res) => {
-    let { difficulty, start, count } = req.query;
+    const { difficulty, start, count } = req.query;
     getWord(difficulty, start, count).then((data) => {
         res.status(200).send(data);
       }  
