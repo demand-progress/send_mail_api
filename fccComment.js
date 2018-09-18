590b41c5ef2870e95f1834fe9c7ea02cbaf7760d
Mateo Balcorta <misterrattail@gmail.com>
2018-09-18 11:45:03 -0700

    Update to return city and state from zipcode.

--- fccComment.js
+++ fccComment.js
@@ -1,21 +1,23 @@
 const axios = require('axios');
 const { parseString } = require('xml2js');
-const stateAbbreviations = require('states-abbreviations');
+const keys = require('./config/key.js');
 
-const getState = zip => new Promise(async (resolve, reject) => {
+const getStateAndCity = zip => new Promise(async (resolve, reject) => {
   axios.get('https://secure.shippingapis.com/ShippingAPI.dll?', {
     params: {
       API: 'CityStateLookup',
-      xml: `<CityStateLookupRequest USERID="661DEMAN2298"><ZipCode ID="0"><Zip5>${zip}</Zip5></ZipCode></CityStateLookupRequest>`
-    }
+      xml: `<CityStateLookupRequest USERID="661DEMAN2298"><ZipCode ID="0"><Zip5>${zip}</Zip5></ZipCode></CityStateLookupRequest>`,
+    },
   })
     .then((response) => {
       const xml = response.data;
+
       parseString(xml, (err, result) => {
-        const zipStateCityObj = result.CityStateLookupResponse.ZipCode[0];
-        const state = zipStateCityObj.State[0];
-        const stateFullName = stateAbbreviations[state];
-        resolve({ stateFullName });
+        const { State, City } = result.CityStateLookupResponse.ZipCode[0];
+        resolve({
+          state: State[0],
+          city: City[0],
+        });
       });
     })
     .catch((error) => {
@@ -23,18 +25,21 @@ const getState = zip => new Promise(async (resolve, reject) => {
     });
 });
 
-const postComment = reqBody => new Promise(
+const postFccComment = reqBody => new Promise(
   async (resolve, reject) => {
     const {
-      first_name, last_name, email, zip, ftc_comment,
+      first_name, 
+      last_name, 
+      email, 
+      address1, 
+      zip, 
+      fcc_comment,
     } = reqBody;
 
-    const cityStateObj = await getState(zip);
-    const { stateFullName } = cityStateObj;
+    const cityStateObj = await getStateAndCity(zip);
+    const { state, city } = cityStateObj;
 
-  }
-
-  axios({
+    axios({
       method: 'post',
       url: `https://publicapi.fcc.gov/ecfs/filings?api_key=${keys.fccKey}`,
       data: {
@@ -53,14 +58,15 @@ const postComment = reqBody => new Promise(
         contact_email: email,
         addressentity: {
           address_line_1: address1,
-          city: 'oakland',
-          state: 'ca',
+          city,
+          state,
           zip_code: zip,
         },
-        text_data: action_fcc_comment,
+        text_data: fcc_comment,
         express_comment: 1,
-      }
-    })
+      },
+    });
+  },
 );
 
 module.exports = postFccComment;
