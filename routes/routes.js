f3546d1ec556c8ac139a65766260c1bb30d425bf
Mateo Balcorta <misterrattail@gmail.com>
2018-10-04 12:41:24 -0700

    Add post route to send email.

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const postFccComment = require('../fccComment.js');
+const sendEmail = require('../confirmationEmail.js'); 
 
 const appRouter = (app) => {
   app.get('/', (req, res) => {
@@ -9,6 +10,13 @@ const appRouter = (app) => {
     postFccComment(req.body)
       .then(answer => res.send(answer))
       .catch(error => console.log(error));
+  });
+
+  app.post('/sendEmail', (req, res) => {
+    const { toEmail } = req.body;
+    sendEmail(toEmail)
+      .then(response => res.send(response))
+      .catch(error => console.log(error))
   });
 };
 module.exports = appRouter;
