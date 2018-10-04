cce9430c47c3c0fe335dbcb858544eaa60b7f9a1
Mateo Balcorta <misterrattail@gmail.com>
2018-10-04 15:31:26 -0700

    Remove fcc comment func.

--- app.js
+++ app.js
@@ -8,15 +8,8 @@ const routes = require('./routes/routes.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let whitelist = ['https://nomobilemegamerger.com', 'http://localhost:8008', 'https://script.google.com/a/demandprogress.org/macros/d/MXrCPMs2Zg5YspN2jGy7A8VnFY-KFaGkg/edit?uiv=2&mid=ACjPJvHfRddN5yxf7EjoXz2pai3GtVnYRhCkX1dWX3gg4dGLqTcZIAxMMmQyVXQyAnknWKnqloFCTLzrJwIbqX1uqVJ8tvUbJkXigttfCr1Kt6RYZr9mHDyHFhh2tJ3pBQw2asEzewCalg'];
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
+  origin: '*',
   credentials: true,
   optionsSuccessStatus: 200,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,8 @@
-const postFccComment = require('../fccComment.js');
 const sendEmail = require('../confirmationEmail.js'); 
 
 const appRouter = (app) => {
   app.get('/', (req, res) => {
     res.send('welcome!');
-  });
-
-  app.post('/fcccomment', (req, res) => {
-    postFccComment(req.body)
-      .then(answer => res.send(answer))
-      .catch(error => console.log(error));
   });
 
   app.post('/sendEmail', (req, res) => {
